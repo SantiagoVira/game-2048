b1283732bda58ad9173de0e4023644a93a901539
SantiagoVira <santiagovira06@gmail.com>
2023-01-16 15:51:34 -0500

    Switch to struct

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,48 @@
 use rand::Rng;
 
 fn main() {
-  let mut state = [[0u8; 4]; 4];
+  let mut board = Board::new();
 
-  fill_square(&mut state);
-
-
-  draw(state)
+  board.fill_square();
+  board.draw();
   
 }
 
-fn fill_square(state: &mut [[u8; 4]; 4]){
-  let mut rng = rand::thread_rng();
-
-  let x = rng.gen_range(0..4);
-  let y = rng.gen_range(0..4);
-  let num = rng.gen_range(1..3) * 2;
-
-  state[y][x] = num;
+struct Board {
+  squares: [[u32; 4]; 4],
 }
 
+impl Board {
+  pub fn new() -> Self {
+    // create the board
+    return Board{squares: [[0u32; 4]; 4]};
+  }  
 
-fn draw(state: [[u8; 4]; 4]) {
-  for y in 0..4{
-    println!("{}", "-".repeat(7 * 4 + 1));
-    for x in 0..4{
-      print!("| {0: <5}", if state[y][x] != 0 {format!("{}", state[y][x])} else {String::new()});
+  // draw shouldnt need to mutate the board, hence an immut. ref to self
+  pub fn draw(&self) {
+    for y in 0..4{
+      println!("{}", "-".repeat(7 * 4 + 1));
+      for x in 0..4{
+        if self.squares[y][x] != 0 {
+          print!("| {0: <5}", self.squares[y][x]);
+        } else {
+          print!("|      ");
+        }
+      }
+      println!("|");
     }
-    println!("|");
+    
+    println!("{}", "-".repeat(7 * 4 + 1));
   }
+
+  // fill_squares will mutate self, hence a mut ref
+  pub fn fill_square(&mut self) {
+    let mut rng = rand::thread_rng();
+
+    let x = rng.gen_range(0..4);
+    let y = rng.gen_range(0..4);
+    let num = rng.gen_range(1..3) * 2;
   
-  println!("{}", "-".repeat(7 * 4 + 1));
+    self.squares[y][x] = num;
+  }
 }
